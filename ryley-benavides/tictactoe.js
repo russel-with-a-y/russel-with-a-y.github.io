a8265b0d3742afeaa5b98bab653a5159db6dc76b
Ryley Benavides <ryley.benavides@student.csulb.edu>
2023-12-06 19:55:26 -0800

    fixtoe

--- ryley-benavides/tictactoe.js
+++ ryley-benavides/tictactoe.js
@@ -1,55 +1,70 @@
+// Add an event listener to the message input field for word count limitation
 document.getElementById('message').addEventListener('input', function () {
     var wordCount = this.value.split(/\s+/).length;
     if (wordCount > 250) {
+        // Limit the input to 250 words
         this.value = this.value.split(/\s+/).slice(0, 250).join(" ");
     }
 });
 
+// Add an event listener to the contact widget button to toggle display
 document.getElementById('contactWidgetButton').addEventListener('click', function() {
     var widget = document.getElementById('contactWidget');
+    // Toggle the display of the contact widget
     widget.style.display = widget.style.display === 'block' ? 'none' : 'block';
 });
 
+// Function to close the contact widget
 function closeContactWidget() {
     document.getElementById('contactWidget').style.display = 'none';
 }
 
+// Add an event listener to the Tic Tac Toe button to toggle game display
 document.getElementById('ticTacToeButton').addEventListener('click', function() {
     var ticTacToeWidget = document.getElementById('ticTacToeWidget');
+    // Toggle the display of the Tic Tac Toe widget
     ticTacToeWidget.style.display = ticTacToeWidget.style.display === 'block' ? 'none' : 'block';
     if (ticTacToeWidget.style.display === 'block') {
+        // Start the game when the widget is displayed
         startGame();
     }
 });
 
+// Define initial game variables
 let currentPlayer = 'X'; // Player is always 'X'
 let gameActive = true;
 let gameState = ["", "", "", "", "", "", "", "", ""];
 
+// Handle cell clicks for player moves
 function handleCellClick(clickedCellEvent) {
     const clickedCell = clickedCellEvent.target;
     const clickedCellIndex = parseInt(clickedCell.getAttribute('data-cell-index'));
 
     if (gameState[clickedCellIndex] !== "" || !gameActive) {
+        // Prevent move if the cell is already taken or the game is not active
         return;
     }
 
+    // Make the player's move
     makeMove(clickedCell, clickedCellIndex, currentPlayer);
     if (!checkForWinner()) {
-        // Delay computer's move by 2 seconds
+        // Delay the computer's move by 2 seconds
         setTimeout(computerMove, 2000); // 2000 milliseconds delay
     }
 }
 
+// Make a move on the game board
 function makeMove(cell, index, player) {
     gameState[index] = player;
     cell.innerHTML = player;
 }
 
+// Function for the computer to make a move
 function computerMove() {
     let availableSpots = gameState.map((cell, index) => cell === "" ? index : null).filter(val => val !== null);
 
     if (availableSpots.length === 0) {
+        // No available spots, exit the function
         return;
     }
 
@@ -59,11 +74,12 @@ function computerMove() {
     checkForWinner();
 }
 
+// Check the game state for a winner
 function checkForWinner() {
     const winningConditions = [
-        [0, 1, 2], [3, 4, 5], [6, 7, 8],
-        [0, 3, 6], [1, 4, 7], [2, 5, 8],
-        [0, 4, 8], [2, 4, 6]
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+        [0, 4, 8], [2, 4, 6]             // Diagonals
     ];
 
     let roundWon = false;
@@ -77,7 +93,7 @@ function checkForWinner() {
         }
         if (a === b && b === c) {
             roundWon = true;
-            break
+            break;
         }
     }
 
@@ -94,24 +110,28 @@ function checkForWinner() {
         return true;
     }
 
+    // Switch current player
     currentPlayer = currentPlayer === "X" ? "O" : "X";
     return false;
 }
 
+// Function to start a new game
 function startGame() {
     gameState = ["", "", "", "", "", "", "", "", ""];
     gameActive = true;
     currentPlayer = "X";
-    document.getElementById('gameBoard').innerHTML = '';
+    const gameBoard = document.getElementById('gameBoard');
+    gameBoard.innerHTML = ''; // Clear the game board
     for (let i = 0; i < 9; i++) {
         let cell = document.createElement('div');
         cell.classList.add('tic-tac-toe-cell');
         cell.setAttribute('data-cell-index', i);
         cell.addEventListener('click', handleCellClick);
-        document.getElementById('gameBoard').appendChild(cell);
+        gameBoard.appendChild(cell);
     }
 }
 
+// Function to reset the game
 function resetGame() {
     startGame();
 }
